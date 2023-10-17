1942199e11ae9875a638140744ae09017f165dd9
bcabreu <bcabreu@icloud.com>
2023-10-17 19:12:18 -0300

    Config Project

---
+++ LoginViewCode-Curso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginViewCode-Curso
+//
+//  Created by Bruno Abreu on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
